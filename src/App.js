46a301b76fcfa47239b76921976849959c001cd0
marwanelhaj <roxelhaj@gmail.com>
2021-02-28 20:44:39 +0100

    updating shop page with collection item and collection perview

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
 
 import Homepage from './components/pages/homepage/homepage.component'
+import ShopPage from './components/pages/shop/shop.component.jsx'
+
 
 function App() {
   return (
     <div>
-     <Homepage />
+     <Switch>
+       <Route exact path='/' component={Homepage} /> 
+       <Route  path='/shop' component={ShopPage} /> 
+     </Switch> 
     </div>
   );
 }
