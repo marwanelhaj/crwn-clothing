6c2dae63901192031a6936d27b9d36738a7b0dfe
marwanelhaj <roxelhaj@gmail.com>
2021-03-30 17:04:02 +0100

    added sign-up

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Homepage from './components/pages/homepage/homepage.component'
 import ShopPage from './components/pages/shop/shop.component.jsx'
 import SignInAndSignUp from './components/pages/sing-in-and-sign-up/sing-in-and-sign-up.component'
 import Header from './components/header/header.component'
-import { auth } from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
  
 
 
@@ -22,17 +22,28 @@ class App extends React.Component {
  unsubscribeFromAuth = null
 
  componentDidMount(){
-   this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-     this.setState({currentUser: user})
+   this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    if(userAuth){
+      const userRef = await createUserProfileDocument(userAuth);
 
-     console.log(user);
+      userRef.onSnapshot(snapShot => {
+        this.setState({
+          currentUser:{
+            id: snapShot.id,
+            ...snapShot.data()
+          }
+        })
+        console.log(this.state);
+      })
+    }
+
+    this.setState({ currentUser: userAuth })
    })
  }
  
  componentWillUnmount(){
    this.unsubscribeFromAuth();
  }
-
 
   render(){
     return (
